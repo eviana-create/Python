597d26b1819a4fa8dc5be6c1a18b156e0e4a4a93
eviana-create <missonzito@outlook.com>
2022-09-19 13:37:47 -0300

    Codigo para calcular media e faltas dos alunos

---
+++ qntd_faltas.py
@@ -0,0 +1,6 @@
+qtde_faltas = int(input("Digite a quantidade de faltas: "))
+media_final = float(input("Digite a média final: "))
+if qtde_faltas <= 5 or media_final >= 6:
+ print("Aluno Aprovado!")
+else:
+ print("Aluno Reprovado!")
