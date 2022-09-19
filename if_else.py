e5dd40c2bde850a784c5746ae523b5fb02da1cd7
eviana-create <missonzito@outlook.com>
2022-09-19 13:06:11 -0300

    Comandos 'if' e 'else'

---
+++ if_else.py
@@ -0,0 +1,15 @@
+numbers = input(f'Digite Numero ')
+numbers2: str = input(f'Digite Numero ')
+
+a = int(numbers)
+b = int(numbers2)
+
+if a < b:
+    print(f'a é menor que b')
+    r = a
+    print(r)
+
+else:
+    print(f'a é maior que b')
+    r = a
+    print(r)
